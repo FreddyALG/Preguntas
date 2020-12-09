1c69353aaf3e55266b67998b5d0e9bc56d3a853d
Freddy <Freddy@FREDDY-PC>
2020-12-08 19:08:00 -0500

    Edicion de compras, ventas y pqr

---
+++ Sigmove1.4/src/java/edu/sigmove/facade/ProductoFacade.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sigmove.facade;
+
+import edu.sigmove.entity.Producto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Freddy
+ */
+@Stateless
+public class ProductoFacade extends AbstractFacade<Producto> implements ProductoFacadeLocal {
+
+    @PersistenceContext(unitName = "Sigmove1.4PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    @Override
+    public List<Producto> listaProdutosporcategoria(int fk_categoria){
+        try {
+            Query qt = em.createQuery("SELECT p FROM Producto p WHERE p.idcategoria.idcategoria = :fk_categoria");
+            qt.setParameter("fk_categoria", fk_categoria);
+            return   qt.getResultList();
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public ProductoFacade() {
+        super(Producto.class);
+    }
+
+    
+    
+}
---
+++ Sigmove1.4/src/java/edu/sigmove/facade/UsuarioFacade.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sigmove.facade;
+
+import edu.sigmove.entity.Usuario;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Freddy
+ */
+@Stateless
+public class UsuarioFacade extends AbstractFacade<Usuario> implements UsuarioFacadeLocal {
+
+    @PersistenceContext(unitName = "Sigmove1.4PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsuarioFacade() {
+        super(Usuario.class);
+    }
+
+    @Override
+    public Usuario recuperarContraseña(String usuarioIn) {
+        try {
+            Query qt = em.createQuery("SELECT u FROM Usuario u WHERE u.usuario = :usuarioIn ");
+            qt.setParameter("usuarioIn", usuarioIn);
+            return (Usuario) qt.getSingleResult();
+        } catch (Exception e) {
+            return new Usuario();
+        }
+
+    }
+     @Override
+    public Usuario loginUsuario(String usuarioIn , String contraseñaIn){
+        try {
+            Query q = em.createQuery("SELECT u FROM Usuario u WHERE u.usuario = :usuarioIn AND u.contraseña = :contraseñaIn");
+            q.setParameter("usuarioIn",usuarioIn );
+            q.setParameter("contraseñaIn",contraseñaIn );
+            return (Usuario) q.getSingleResult();            
+        } catch (Exception e) {
+        return  new Usuario();
+        }
+    }
+    
+    
+    
+}
